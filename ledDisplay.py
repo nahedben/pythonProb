b7ab816023f99f56b5b47d5cf043e2d4e50719b2
nahedben <59153046+nahedben@users.noreply.github.com>
2022-04-04 11:21:28 +0300

    Add files via upload
    python file

---
+++ ledDisplay.py
@@ -0,0 +1,37 @@
+## Display stars in number shapes
+
+baselist={'0':['# # #','#   #','#   #','#   #','# # #'],
+          '1':['   # ','   # ','   # ','   # ','   # '],
+          '2':['# # #','    #','# # #','#    ','# # #'],
+          '3':['# # #','    #','# # #','    #','# # #'],
+          '4':['#   #','#   #','# # #','    #','    #'],
+          '5':['# # #','#    ','# # #','    #','# # #'],
+          '6':['# # #','#    ','# # #','#   #','# # #'],
+          '7':['# # #','    #','    #','    #','    #'],
+          '8':['# # #','#   #','# # #','#   #','# # #'],
+          '9':['# # #','#   #','# # #','    #','# # #']}
+
+def disp(l): 
+  for n in range(5):
+    print()
+    for i in l:
+      print(i[n],end='  ')
+  print()
+
+def sevenSegment(num):
+  resl=[]
+  l=[]
+  if type(num)!='string':
+    num =str(num)
+  
+  for e in num :
+    l.append(e)
+
+  for n in l:
+    resl.append(baselist[n])
+
+  return disp(resl)
+
+   
+
+sevenSegment(9457102)
